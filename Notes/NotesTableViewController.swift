45a8643ce3584532e8a733a6f10ccc390ecc7360
fercuacho <keroro@live.com.mx>
2023-10-02 10:59:13 -0600

    Revert "pequeñas correcciones"
    This reverts commit 3c0b1513845367e03b535cdeabc1a0cb32db8a2d.

--- Notes/NotesTableViewController.swift
+++ Notes/NotesTableViewController.swift
@@ -8,39 +8,38 @@
 import UIKit
 
 class NotesTableViewController: UITableViewController {
-
-    @IBOutlet var tableViewNotes: UITableView!
-    @IBOutlet var emptyNotesView: UIView!
-    
     
     let noteManager = NoteManager()
-
-    var note : Note?
     
+    @IBOutlet var tableViewNotes: UITableView!
+    @IBOutlet var emptyNotesView: UIView!
+    var note: Note?
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if noteManager.countNotes() == 0 {
             emptyNotesView.isHidden = false
             tableViewNotes.backgroundView = emptyNotesView
         }
-        else{
+        else {
             emptyNotesView.isHidden = true
-            tableViewNotes.reloadData()        }
+        }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return noteManager.countNotes()
     }
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Configure the cell...
         let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath) as! NoteCell
@@ -48,76 +47,57 @@ class NotesTableViewController: UITableViewController {
         cell.noteDate.text = noteManager.getNote(at: indexPath.row).date.iso8601String
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            //Delete ...
-            print("Delete...")
-            noteManager.deleteNote(at: indexPath.row)
-            tableViewNotes.reloadData()        }
-    }
-    
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    
-    @IBAction func unWindFromAddNoteViewController(segue: UIStoryboardSegue){
-        print("Unwind segue!")
-        let source = segue.source as! AddNoteViewController
-        note = source.newNote
-        noteManager.createNote(note: note!)
-        print("#",noteManager.countNotes())
-        print("notes:",noteManager.getNotes())
-        noteManager.saveNotes()
-        tableViewNotes.reloadData()
-    }
-    
-    /*
-    @IBAction func unWindToNoteWithSegue(segue: UIStoryboardSegue){
-        print("Unwind segue!")
-        let source = segue.source as! AddNoteViewController
-        note = source.newNote
-        noteManager.createNote(note: note!)
-        print("#",noteManager.countNotes())
-        print("notes:",noteManager.getNotes())
-        noteManager.saveNotes()
-        tableViewNotes.reloadData()
+        /*
+         // Override to support conditional editing of the table view.
+         override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+         // Return false if you do not want the specified item to be editable.
+         return true
+         }
+         */
         
-    }*/
+        
+    // Override to support editing the table view.
+        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                //Delete ...
+                print("Delete...")
+            }
+        }
+         
+        
+        /*
+         // Override to support rearranging the table view.
+         override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+         
+         }
+         */
+        
+        /*
+         // Override to support conditional rearranging of the table view.
+         override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+         // Return false if you do not want the item to be re-orderable.
+         return true
+         }
+         */
+        
+        
+         // MARK: - Navigation
+         
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         
+        @IBAction func unWindToNote (segue: UIStoryboardSegue){
+            let source = segue.source as! AddNoteViewController
+            note = source.newNote
+            noteManager.createNote(note: note!)
+            print("#", noteManager.countNotes())
+            print("notes: ", noteManager.getNotes())
+            noteManager.saveNotes()
+            tableViewNotes.reloadData()
+        }
+        
+        
     
-    
-
 }
-
